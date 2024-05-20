c7c1c2a2c33f3b5a78ce0670b320c428461e3d2b
HilbertoS <114943746+HilbertoS@users.noreply.github.com>
2024-05-20 16:36:04 +0200

    Update helloWord.java
    cuatro

--- helloWord.java
+++ helloWord.java
@@ -1,6 +1,6 @@
 public class helloWord {
 
     public static void main(String[] args) {
-        System.out.println("Hello World, Hello Jekinssss!!!")safdadfasdas
+        System.out.println("Hello World, Hello Jekinssss!!!");
     }
 }
